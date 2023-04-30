286192b1cd84ad7241654c8c190f8e809eee8106
Lewis Moore <lewismooredev@gmail.com>
2023-04-30 13:37:28 +0100

    Update gitignore

--- PokemonBattler/Program.cs
+++ PokemonBattler/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace PokemonBattler;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
