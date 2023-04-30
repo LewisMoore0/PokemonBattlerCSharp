8bdc43514118035c3f56d85180226d1475807545
Lewis Moore <lewismooredev@gmail.com>
2023-04-30 14:21:19 +0100

    Add first test

---
+++ PokemonBattler.Tests/PokemonTests/PokemonTests.cs
@@ -0,0 +1,18 @@
+﻿using PokemonBattler.Pokemon;
+using Xunit;
+using FluentAssertions;
+
+namespace PokemonBattler.Tests.PokemonTests;
+
+public class PokemonTest
+{
+    [Fact]
+    public void shouldHaveNameProperty()
+    {
+        var lewis = new PokemonClass();
+        var result = lewis.testFunction();
+
+        result.Should().Be("Hi!");
+    }
+}
+
---
+++ PokemonBattler/Pokemon/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace PokemonBattler.Pokemon
+{
+	public class PokemonClass
+	{
+		public PokemonClass()
+		{
+		}
+
+		public string testFunction()
+		{
+			return "Hi!";
+		}
+	}
+}
+
